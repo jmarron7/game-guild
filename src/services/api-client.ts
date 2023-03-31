cb83d91af796b78d13c6c5bdb79c896a7b7bd343
Jesus Marron <jesusmarron7@gmail.com>
2023-03-31 17:23:02 -0500

    separate http request logic into useGames hook

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const apiUrl: string = import.meta.env.VITE_API_URL
-const apiKey: string = import.meta.env.VITE_API_KEY
+const apiUrl = import.meta.env.VITE_API_URL
+const apiKey = import.meta.env.VITE_API_KEY
 
 
 export default axios.create({
