472f82e24116883d5d712ee0afd816c7e7d97298
Jesus Marron <jesusmarron7@gmail.com>
2023-04-03 14:48:49 -0500

    display genres

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,10 +3,11 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 const GameCardSkeleton = () => {
   return (
     <Card>
-      <Skeleton height="200px" />
-      <CardBody>
-        <SkeletonText />
-      </CardBody>
+      <Skeleton height="200px">
+        <CardBody>
+          <SkeletonText />
+        </CardBody>
+      </Skeleton>
     </Card>
   );
 };
